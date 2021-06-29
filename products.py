8c0bf1a194bbe7ba7f7addf5ede0b44106d3332b
Ray Chen <rayallen1996xd@gmail.com>
2021-06-29 17:56:19 +0800

    new

--- products.py
+++ products.py
@@ -14,8 +14,19 @@ print(products)
 for p in products:
 	print(p[0],'的價格是：',p[1])
 #p[0]:all products, p[1]:all price
-with open ('products.csv','w', encoding = 'utf-8') as f:
+with open ('products.csv','w',
+ encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n' )
 #寫入csc檔，＋號連接字串，encoding = utf-8中文字，\n換行
+#讀取檔案p
+products = []
+with open('products.csv','r',encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #回去上一段繼續，通常寫在高位，一開始就略過
+		name,price = line.strip().split(',')
+		products.append([name, price])
+	print(products)
+
