b024d2cd9e343d3bf5c2602ef55f68eac8eee4c7
Ray Chen <rayallen1996xd@gmail.com>
2021-06-29 17:34:31 +0800

    header

--- products.py
+++ products.py
@@ -14,7 +14,8 @@ print(products)
 for p in products:
 	print(p[0],'的價格是：',p[1])
 #p[0]:all products, p[1]:all price
-with open ('products.csv','w') as f:
+with open ('products.csv','w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n' )
-#寫入csc檔，＋號連接字串，\n換行
+#寫入csc檔，＋號連接字串，encoding = utf-8中文字，\n換行
