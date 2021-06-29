c4740be5aecbe8d53c425815e6a57b024f7c0358
Ray Chen <rayallen1996xd@gmail.com>
2021-06-29 20:01:52 +0800

    file check

--- products.py
+++ products.py
@@ -1,14 +1,19 @@
-#讀取檔案
+import os #operating system
 products = []
-with open('products.csv','r',encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #回去上一段繼續，通常寫在高位，一開始就略過
-		name,price = line.strip().split(',')
-		products.append([name, price])
-	print(products)
+if os.path.isfile('products.csv'): #確認檔案是否存在
+	print('檔案存在')
+	with open('products.csv','r',encoding = 'utf-8') as f: #讀取檔案
+		for line in f:
+			if '商品,價格' in line:
+				continue #回去上一段繼續，通常寫在高位，一開始就略過
+			name,price = line.strip().split(',')
+			products.append([name, price])
+		print(products)
+else:
+	print('找不到檔案')
 
-#不知道輸入幾次用while，反之用for
+
+#讓使用者輸入
 products = []
 while True:
 	name = input('請輸入商品名稱：')
@@ -20,12 +25,12 @@ while True:
 	p.append(price)
 	products.append(p)#二維清單
 print(products)
-
+#印出所有購買紀錄
 for p in products:
 	print(p[0],'的價格是：',p[1])
 #p[0]:all products, p[1]:all price
-with open ('products.csv','w',
- encoding = 'utf-8') as f:
+#寫入檔案
+with open ('products.csv','w',encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n' )
